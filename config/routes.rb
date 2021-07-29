583c652a9643b5dd26a66e4c9bd569e3e25838e4
Anna Ying <annayingwork@gmail.com>
2021-07-29 15:48:33 +0800

    changed front a bit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.htm
-  get 'harmonies/chordify', to: "harmonies#chordify", as: 'chordify'
+  # get 'harmonies/chordify', to: "harmonies#chordify", as: 'chordify'
 
   get 'major_triad', to: "harmonies#major_triad", as: 'major_triad'
   get 'tritone', to: "harmonies#tritone", as: 'tritone'
