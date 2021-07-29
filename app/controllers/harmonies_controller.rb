06c4f462417b20655587388e7dd9eed23142c01a
Anna Ying <annayingwork@gmail.com>
2021-07-29 14:34:07 +0800

    set up routes, harmonies controller

---
+++ app/controllers/harmonies_controller.rb
@@ -0,0 +1,29 @@
+class HarmoniesController < ApplicationController
+  def chordify
+    @root = params[:number].to_i
+    @notes = Note.all
+
+    chord_type = params[:chord_type]
+    if chord_type == "major_triad"
+      major_triad
+    elsif chord_type == "tritone"
+      tritone
+    else
+      @results = ["This chord is not yet ready to be played"]
+    end
+  end
+
+  private
+
+  def major_triad
+    # @notes = Note.all
+    # @root = params[:number].to_i
+    @results = [Note.find_by_number(@root), Note.find_by_number(@root + 4),Note.find_by_number(@root + 7)]
+  end
+
+  def tritone
+    # @notes = Note.all
+    # @root = params[:number].to_i
+    @results = [Note.find_by_number(@root),Note.find_by_number(@root + 6)]
+  end
+end
