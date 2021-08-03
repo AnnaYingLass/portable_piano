a0bc98ad8e39689d46a2f5ba7c53652ff5700899
Anna Ying <annayingwork@gmail.com>
2021-08-04 02:01:26 +0800

    inversion

--- app/javascript/scripts/popup.js
+++ app/javascript/scripts/popup.js
@@ -5,18 +5,18 @@ const popUp = () => {
     const open = document.getElementById('open');
     const videobox = document.getElementById('videobox');
     const close = document.getElementById('close');
-    console.log(open);
-    console.log(videobox);
-    console.log(close);
+    //console.log(open);
+    //console.log(videobox);
+    //console.log(close);
 
     open.addEventListener('click', () => {
       videobox.classList.add('show');
-      console.log("open clicked");
+      //console.log("open clicked");
     });
 
     close.addEventListener('click', () => {
       videobox.classList.remove('show');
-      console.log("close clicked")
+      //console.log("close clicked")
     })
 
   }
