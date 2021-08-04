b4fb9680ac78f5c35a9690085db131140d7cae96
Anna Ying <annayingwork@gmail.com>
2021-08-04 13:51:51 +0800

    def gome end

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,4 @@
 class PagesController < ApplicationController
-  # def home
-  #   @notes = Note.all
-  #   @first=Note.find_by_number(1)
-  #   @third= Note.find_by_number(5)
-  # end
+  def home
+  end
 end
