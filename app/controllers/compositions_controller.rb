2c2d4d822a346b4a33ac210e3f0c7ae790acee4e
Anna Ying <annayingwork@gmail.com>
2021-08-06 05:33:02 +0800

    still thinking controller

--- app/controllers/compositions_controller.rb
+++ app/controllers/compositions_controller.rb
@@ -12,13 +12,18 @@ class CompositionsController < ApplicationController
 
   def save_compose
 
-    @my_name_sequence = params[:my_theme]
-    p @my_sname_sequence
-    # above gives an array of note names e,g, [C3,E3,G3]
+    p params[:my_theme]
+    # above gives an array of note names e,g, [C3,E3,G3], from key.dataset.note;
     # change to array of note instances e.g [#< name:'C3'...>, #< name:'E3'...>, #<name: 'G3'>]
-    @my_note_sequence = @my_name_sequence.map{ |name| Note.find_by_name(name) }
-    end
-    render json: @my_saved_theme
+    @my_note_sequence = params[:my_theme].map{ |note| Note.find_by_name(note) }
+
+    # save to database: theme_1 by storing all the theme notes in sequence
+
+    # to retrieve: SELECT * FROM theme_notes WHERE theme_id = 1 ORDER BY rank;
+    # returns eventually,[C3, E3, G3]
+
+
+    render json: @my_note_sequence
   end
 
 end
